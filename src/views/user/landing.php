17d812be44fc553afc185c2a654264380cb539ef
flammadc <acc.for.m33@gmail.com>
2025-01-12 18:05:42 +0700

    feat: navbar

---
+++ src/views/user/landing.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TiketinAja</title>
+    <link href="../../assets/css/output.css" rel="stylesheet">
+
+</head>
+
+<body>
+    <header class="bg-white">
+        <div class="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
+            <div class="flex h-16 items-center justify-between">
+                <div class="md:flex md:items-center md:gap-12">
+                    <a class="block text-teal-600" href="#">
+                        <h1 class="h-8 font-semibold text-xl">TiketinAja</h1>
+                    </a>
+                </div>
+
+
+
+                <div class="flex items-center gap-4">
+                    <div class="sm:flex sm:gap-4">
+                        <a
+                            class="rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white shadow"
+                            href="#">
+                            Login
+                        </a>
+
+                        <div class="hidden sm:flex">
+                            <a
+                                class="rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-teal-600"
+                                href="#">
+                                Register
+                            </a>
+                        </div>
+                    </div>
+
+                    <div class="block md:hidden">
+                        <button class="rounded bg-gray-100 p-2 text-gray-600 transition hover:text-gray-600/75">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                class="size-5"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                                stroke-width="2">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" />
+                            </svg>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+</body>
+
+</html>
