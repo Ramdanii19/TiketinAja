215adeac529319e9acb65e0498645235a6a8899a
flammadc <acc.for.m33@gmail.com>
2025-01-15 09:12:32 +0700

    style: responsive landing

--- src/views/user/landing.php
+++ src/views/user/landing.php
@@ -6,193 +6,86 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TiketinAja</title>
     <link href="../../assets/css/output.css" rel="stylesheet">
-
 </head>
 
 <body>
-    <header class="bg-white">
+    <header class="bg-white shadow">
         <div class="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
             <div class="flex h-16 items-center justify-between">
-                <div class="md:flex md:items-center md:gap-12">
-                    <a class="block text-indigo-600" href="#">
-                        <h1 class="h-8 font-semibold text-xl">TiketinAja</h1>
-                    </a>
-                </div>
-
-
-
+                <a class="block text-indigo-600" href="#">
+                    <h1 class="text-xl font-semibold">TiketinAja</h1>
+                </a>
                 <div class="flex items-center gap-4">
-                    <div class="sm:flex sm:gap-4">
-                        <a
-                            class="rounded-md bg-indigo-600 px-5 py-2.5 text-sm font-medium text-white shadow"
-                            href="#">
-                            Login
-                        </a>
-
-                        <div class="hidden sm:flex">
-                            <a
-                                class="rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-indigo-600"
-                                href="#">
-                                Register
-                            </a>
-                        </div>
+                    <div class="hidden sm:flex sm:gap-4">
+                        <a class="rounded-md bg-indigo-600 px-5 py-2.5 text-sm font-medium text-white hover:bg-indigo-700 border border-indigo-600" href="#">Login</a>
+                        <a class="rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-indigo-600 hover:bg-gray-200 border border-gray-200" href="#">Register</a>
                     </div>
-
-                    <div class="block md:hidden">
-                        <button class="rounded bg-gray-100 p-2 text-gray-600 transition hover:text-gray-600/75">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                class="size-5"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke="currentColor"
-                                stroke-width="2">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" />
-                            </svg>
-                        </button>
-                    </div>
+                    <button class="block rounded bg-gray-100 p-2 text-gray-600 transition hover:text-gray-700 border border-gray-200 md:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                        </svg>
+                    </button>
                 </div>
             </div>
         </div>
     </header>
 
-    <div class="container min-w-full flex items-center justify-center">
-        <section
-            class="w-full  overflow-hidden bg-[url(https://images.unsplash.com/photo-1489987707025-afc232f7ea0f?q=80&w=2670&auto=format&fit=crop)] bg-cover bg-top bg-no-repeat">
-            <div class=" bg-black/70 p-8 md:p-12 lg:px-96  lg:py-24">
-                <div class="text-center ltr:sm:text-left rtl:sm:text-right">
+    <main class="mx-auto">
+        <section class="relative w-full bg-cover bg-center bg-no-repeat "
+            style="background-image: url('https://images.unsplash.com/photo-1632099058914-1d1c64915d0b?q=80&w=1971&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');">
+            <div class="bg-black/70 px-8 py-12 md:px-12 lg:px-72 lg:py-24">
+                <div class="text-center">
                     <h2 class="text-2xl font-bold text-white sm:text-3xl md:text-5xl">Pesan Tiket Sekarang!</h2>
-
-                    <fieldset class="w-1/2 grid grid-cols-2 gap-4 mt-8">
-                        <legend class="sr-only">Booking</legend>
-
-                        <div>
-                            <label
-                                for="DeliveryStandard"
-                                class="flex cursor-pointer justify-between gap-4 rounded-lg border border-gray-100 bg-white px-4 py-3 text-xs font-medium shadow-sm hover:border-gray-200 has-[:checked]:border-blue-500 has-[:checked]:ring-1 has-[:checked]:ring-blue-500">
-                                <div>
-                                    <p class="text-gray-700">Pulang-Pergi</p>
-
-                                </div>
-
-                                <input
-                                    type="radio"
-                                    name="DeliveryOption"
-                                    value="DeliveryStandard"
-                                    id="DeliveryStandard"
-                                    class="size-4 border-gray-300 text-blue-500"
-                                    checked />
-                            </label>
-                        </div>
-
-                        <div>
-                            <label
-                                for="DeliveryPriority"
-                                class="flex cursor-pointer justify-between gap-4 rounded-lg border border-gray-100 bg-white px-4 py-3 text-xs font-medium shadow-sm hover:border-gray-200 has-[:checked]:border-blue-500 has-[:checked]:ring-1 has-[:checked]:ring-blue-500">
-                                <div>
-                                    <p class="text-gray-700">Sekali Jalan</p>
-
-                                </div>
-
-                                <input
-                                    type="radio"
-                                    name="DeliveryOption"
-                                    value="DeliveryPriority"
-                                    id="DeliveryPriority"
-                                    class="size-4 border-gray-300 text-blue-500" />
-                            </label>
-                        </div>
+                    <fieldset class="mt-8 flex flex-col items-center gap-4 sm:flex-row sm:justify-center">
+                        <label class="flex items-center gap-2 text-white">
+                            <input type="radio" name="flightType" class="text-blue-500 border border-gray-300" checked />
+                            Pulang-Pergi
+                        </label>
+                        <label class="flex items-center gap-2 text-white">
+                            <input type="radio" name="flightType" class="text-blue-500 border border-gray-300" />
+                            Sekali Jalan
+                        </label>
                     </fieldset>
 
-                    <article class="flex items-center justify-center flex-col gap-5 rounded-xl bg-white p-4 ring ring-indigo-50 sm:p-6 lg:p-8 mt-3">
-                        <div class="flex items-center justify-center sm:gap-8 lg:gap-2">
-                            <div class="flex items-center justify-between gap-4 rounded-lg border border-gray-100 bg-white px-4 py-3 text-base font-medium shadow-sm hover:border-gray-200 has-[:checked]:border-blue-500 has-[:checked]:ring-1 has-[:checked]:ring-blue-500">
-                                <select
-                                    name="HeadlineAct"
-                                    id="HeadlineAct"
-                                    class="mt-1.5 w-full rounded-lg border-gray-300 text-gray-700 sm:text-sm">
-                                    <option value="">Bandara Asal</option>
-                                    <optgroup label="Jakarta">
-                                        <option value="AK">SOEKARNO HATTA</option>
-                                        <option value="AK">HALIM PERDANA KUSUMA</option>
-                                    </optgroup>
-                                    <optgroup label="Bali">
-                                        <option value="AK">I GUSTI NGURAH RAI</option>
-                                    </optgroup>
-                                    <optgroup label="Yogyakarta">
-                                        <option value="AK">YOGYAKARTA</option>
-                                    </optgroup>
-                                    <optgroup label="Jawa Barat">
-                                        <option value="AK">KERTAJATI</option>
-                                    </optgroup>
-
-                                </select>
-                            </div>
-                            <a
-                                class="inline-block rounded-full border border-indigo-600 bg-indigo-600 p-4 text-white hover:bg-transparent hover:text-indigo-600 focus:outline-none focus:ring active:text-indigo-500"
-                                href="#">
-                                <span class="sr-only"> Switch </span>
-
-
-                                <svg
-                                    class="size-5 rtl:rotate-180"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                    stroke="currentColor">
-                                    <path
-                                        stroke-linecap="round"
-                                        stroke-linejoin="round"
-                                        stroke-width="2"
-                                        d="M18 10L21 7M21 7L18 4M21 7H7M6 14L3 17M3 17L6 20M3 17H17" />
+                    <article class="mt-6 rounded-lg bg-white p-6 shadow-lg border border-gray-200">
+                        <div class="flex flex-wrap flex-grow-0 gap-4 justify-center">
+                            <select class="w-full sm:w-auto rounded-md border-gray-300 px-4 py-2 text-gray-700 sm:text-sm border">
+                                <option value="">Bandara Asal</option>
+                                <optgroup label="Jakarta">
+                                    <option value="SH">Soekarno Hatta</option>
+                                    <option value="HP">Halim Perdanakusuma</option>
+                                </optgroup>
+                                <optgroup label="Bali">
+                                    <option value="NR">Ngurah Rai</option>
+                                </optgroup>
+                            </select>
+                            <button class="hidden md:block rounded-full border border-indigo-600 bg-indigo-600 p-3 text-white hover:bg-transparent hover:text-indigo-600 ">
+                                <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18 10L21 7M21 7L18 4M21 7H7M6 14L3 17M3 17L6 20M3 17H17" />
                                 </svg>
-                            </a>
-                            <div class="flex items-center justify-between gap-4 rounded-lg border border-gray-100 bg-white px-4 py-3 text-base font-medium shadow-sm hover:border-gray-200 has-[:checked]:border-blue-500 has-[:checked]:ring-1 has-[:checked]:ring-blue-500">
-                                <select
-                                    name="HeadlineAct"
-                                    id="HeadlineAct"
-                                    class="mt-1.5 w-full rounded-lg border-gray-300 text-gray-700 sm:text-sm">
-                                    <option value="">Bandara Tujuan</option>
-                                    <optgroup label="Jakarta">
-                                        <option value="AK">SOEKARNO HATTA</option>
-                                        <option value="AK">HALIM PERDANA KUSUMA</option>
-                                    </optgroup>
-                                    <optgroup label="Bali">
-                                        <option value="AK">I GUSTI NGURAH RAI</option>
-                                    </optgroup>
-                                    <optgroup label="Yogyakarta">
-                                        <option value="AK">YOGYAKARTA</option>
-                                    </optgroup>
-                                    <optgroup label="Jawa Barat">
-                                        <option value="AK">KERTAJATI</option>
-                                    </optgroup>
-
-                                </select>
-                            </div>
-                            <div class="flex cursor-pointer justify-between gap-4 rounded-lg border border-gray-100 bg-white px-4 py-3 text-base font-medium shadow-sm hover:border-gray-200 has-[:checked]:border-blue-500 has-[:checked]:ring-1 has-[:checked]:ring-blue-500">
-                                <input type="date">
-                            </div>
-                            <div class="flex text-center cursor-pointer justify-between gap-4 rounded-lg border border-gray-100 bg-white px-4 py-3 text-base font-medium shadow-sm hover:border-gray-200 has-[:checked]:border-blue-500 has-[:checked]:ring-1 has-[:checked]:ring-blue-500">
-                                <input type="number" placeholder="Jumlah Penumpang">
-                            </div>
-
-                            <a
-                                class="inline-block rounded bg-indigo-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-indigo-700 focus:outline-none focus:ring "
-                                href="penerbanganTersedia.php">
+                            </button>
+                            <select class="w-full sm:w-auto rounded-md border-gray-300 px-4 py-2 text-gray-700 sm:text-sm border">
+                                <option value="">Bandara Tujuan</option>
+                                <optgroup label="Yogyakarta">
+                                    <option value="YK">Yogyakarta</option>
+                                </optgroup>
+                                <optgroup label="Jawa Barat">
+                                    <option value="KJ">Kertajati</option>
+                                </optgroup>
+                            </select>
+                            <input type="date" class="w-full sm:w-auto rounded-md border-gray-300 px-4 py-2 text-gray-700 sm:text-sm border" />
+                            <input type="number" placeholder="Jumlah Penumpang" class="w-full sm:w-auto rounded-md border-gray-300 px-4 py-2 text-gray-700 sm:text-sm border" />
+                        </div>
+                        <div class="mt-4 flex justify-center">
+                            <a class="rounded bg-indigo-600 px-6 py-2 text-sm font-medium text-white hover:bg-indigo-700 border border-indigo-600" href="penerbanganTersedia.php">
                                 Cari Sekarang
                             </a>
                         </div>
-
                     </article>
-
-
-
-
                 </div>
             </div>
         </section>
-    </div>
-
+    </main>
 </body>
 
 </html>
