6148499e3c337b0960feae8895be62502d3a2e04
flammadc <acc.for.m33@gmail.com>
2025-01-21 21:07:44 +0700

    feat: show booked flights

--- src/views/user/listPesanan.php
+++ src/views/user/listPesanan.php
@@ -1,3 +1,15 @@
+<?php
+include "../../config/koneksi.php";
+$user_id = 2;
+
+$queryListPesanan = "SELECT * FROM bookings WHERE user_id = '$user_id'";
+$resultListPesanan = mysqli_query($conn, $queryListPesanan);
+$penerbanganData = [];
+while ($dataPesanan = mysqli_fetch_array($resultListPesanan, MYSQLI_ASSOC)) {
+    $pesananData[] = $dataPesanan;
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -39,31 +51,50 @@
                     </h1>
                 </div>
 
-                <div class="overflow-x-auto rounded-lg shadow-lg bg-white lg:col-span-4">
-                    <table class="min-w-full divide-y divide-gray-200">
-                        <thead class="bg-gray-50">
-                            <tr>
-                                <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Maskapai</th>
-                                <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Waktu</th>
-                                <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Durasi</th>
-                                <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Harga</th>
-                                <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Aksi</th>
-                            </tr>
-                        </thead>
-                        <tbody class="bg-white divide-y divide-gray-200">
-                            <tr>
-                                <td class="px-6 py-4 whitespace-nowrap">
-                                    <img class="w-16" src="https://logos-world.net/wp-content/uploads/2023/01/Garuda-Indonesia-Logo.jpg" alt="Garuda">
-                                </td>
-                                <td class="px-6 py-4 whitespace-nowrap">04:25</td>
-                                <td class="px-6 py-4 whitespace-nowrap">7 Jam</td>
-                                <td class="px-6 py-4 whitespace-nowrap">Rp 2.023.850</td>
-                                <td class="px-6 py-4 whitespace-nowrap">
-                                    <a href="detailPesanan.php" class="px-4 py-2 rounded bg-indigo-600 text-white hover:bg-indigo-700">View</a>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
+                <div class="lg:col-span-4">
+                    <?php if (count($pesananData) > 0) {
+                    ?>
+
+                        <div class="flex flex-col gap-4 ">
+                            <?php foreach ($pesananData as $dataPesanan) {
+                                $detail_penerbangan = json_decode($dataPesanan['detail_penerbangan'], true); // Decode JSON into an associative array
+
+                            ?>
+                                <a href="#"
+                                    class=" cursor-pointer shadow-sm flex items-start sm:items-center justify-between flex-col sm:flex-row gap-5 rounded-lg border border-gray-100 bg-white p-6">
+                                    <div class="flex items-center gap-4 w-64">
+                                        <img class="w-12" src="../../assets/img/<?php echo $detail_penerbangan['maskapai'] ?>.png" alt="Garuda">
+                                        <div>
+                                            <p class="text-sm  font-medium text-gray-700"><?php echo $dataPesanan['booking_code']; ?></p>
+                                        </div>
+                                    </div>
+
+                                    <div class="flex gap-3">
+                                        <p class="font-semibold  text-gray-800"><?php echo $detail_penerbangan['asal'] ?></p>
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" />
+                                        </svg>
+
+                                        <p class="font-semibold  text-gray-800"><?php echo $detail_penerbangan['tujuan'] ?></p>
+                                    </div>
+                                    <?php
+                                    if ($dataPesanan['status'] == 'confirmed') { ?>
+                                        <p class="flex place-self-end focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"><?php echo $dataPesanan['status'] ?></p>
+                                    <?php
+                                    } else { ?>
+
+                                        <p class="flex place-self-end  focus:outline-none focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 bg-gray-500 text-white border-gray-600 hover:bg-gray-700 hover:border-gray-600 focus:ring-gray-700"><?php echo $dataPesanan['status'] ?></p>
+                                    <?php
+                                    }
+                                    ?>
+                                </a>
+                            <?php } ?>
+                        </div>
+                    <?php } else { ?>
+                        <div class="bg-white p-4 text-gray-700 rounded-md">
+                            <p class="text-center text-sm font-medium">Penerbangan Tidak Tersedia</p>
+                        </div>
+                    <?php } ?>
                 </div>
             </div>
         </div>
