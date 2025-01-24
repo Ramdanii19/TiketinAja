be2eff8eaf48cff704f9dcebee4dc25ba3581aa8
Cup_Read <hafidh.10523009@mahasiswa.unikom.ac.id>
2025-01-24 16:06:15 +0700

    Update login.php

--- src/views/Auth/login.php
+++ src/views/Auth/login.php
@@ -45,7 +45,7 @@ if (isset($_POST['login'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link href="css/output.css" rel="stylesheet">
+    <link href="src/assets/css/output.css" rel="stylesheet">
 </head>
 <body class="bg-gray-100 flex items-center justify-center min-h-screen">
 
