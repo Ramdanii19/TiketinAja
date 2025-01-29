653d99b32250a37c9e1a5ca574c9250038cabd2f
Ramdani <110803481+Ramdanii19@users.noreply.github.com>
2025-01-29 21:22:44 +0700

    update: create pesawa unfinished

--- src/views/admin/pesawat/dashboard.php
+++ src/views/admin/pesawat/dashboard.php
@@ -260,11 +260,11 @@ $count = mysqli_num_rows($result);
                             </div>
                             <div class="col-span-2">
                                 <label for="keberangkatan" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Keberangkatan</label>
-                                <input type="date" name="keberangkatan" id="keberangkatan" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="********" required="">
+                                <input type="datetime-local" name="keberangkatan" id="keberangkatan" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="********" required="">
                             </div>
                             <div class="col-span-2">
                                 <label for="kedatangan" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Kedatangan</label>
-                                <input type="date" name="kedatangan" id="kedatangan" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="********" required="">
+                                <input type="datetime-local" name="kedatangan" id="kedatangan" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="********" required="">
                             </div>
                             <div class="col-span-2">
                                 <label for="harga" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Harga</label>
@@ -281,7 +281,7 @@ $count = mysqli_num_rows($result);
                         </div>
                         <button type="submit" name="submit" class="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                             <svg class="me-1 -ms-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd"></path></svg>
-                            Add new product
+                            Tambah Data Pesawat
                         </button>
                     </form>
                     <?php
@@ -295,8 +295,8 @@ $count = mysqli_num_rows($result);
                       $price = $_POST["harga"];
                       $kursi = $_POST["kursi"];
                       $jadwal = $_POST["jadwal"];
-                      $data = array('jadwal' => $jadwal);
-                      $operasional = json_encode($data);
+                      $jadwalArray = explode(", ", $jadwal); 
+                      $operasional = json_encode($jadwalArray); 
 
                       $insertPesawat = "INSERT INTO pesawat (nomor_penerbangan, maskapai, asal, tujuan, waktu_keberangkatan, waktu_kedatangan, price, kursi, operasional) VALUES ('$nomor', '$maskapai', '$asal', '$tujuan', '$keberangkatan', '$kedatangan', '$price', '$kursi', '$operasional')";
                       $queryPesawat = mysqli_query($conn, $insertPesawat);
@@ -305,88 +305,13 @@ $count = mysqli_num_rows($result);
                         echo"<script>alert('Daftar Berhasil Disimpan !') </script>";  
                         echo"<script type='text/javascript'>window.location = 'dashboard.php'</script>";  
                       } else {
-                        echo"<script>alert('Daftar Gagal Disimpan !') </script>";  
-                        echo"<script type='text/javascript'>window.location = 'dashboard.php'</script>";  
+                        die("Error: " . mysqli_error($conn));
                       }
                     }
                     ?>
                 </div>
             </div>
-        </div> 
-
-        <!-- Update modal -->
-        <div id="edit-modal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
-            <div class="relative p-4 w-full max-w-md max-h-full">
-                <!-- Modal content -->
-                <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-                    <!-- Modal header -->
-                    <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
-                        <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
-                            Update Data Pesawat
-                        </h3>
-                        <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="create-modal">
-                            <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
-                            </svg>
-                            <span class="sr-only">Close modal</span>
-                        </button>
-                    </div>
-                    <!-- Modal body -->
-                    <?php
-                      if(!isset($_POST['submit'])) {
-                    ?>
-                    <form class="p-4 md:p-5" method="POST">
-                        <div class="grid gap-4 mb-4 grid-cols-2">
-                            <div class="col-span-2">
-                                <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nama</label>
-                                <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Masukan Nama" required="">
-                            </div>
-                            <div class="col-span-2">
-                                <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
-                                <input type="text" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="example123@gmail.com" required="">
-                            </div>
-                            <div class="col-span-2">
-                                <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Passoword</label>
-                                <input type="password" name="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="********" required="">
-                            </div>
-                            <div class="col-span-2">
-                                <label for="role" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Role</label>
-                                <select id="role" name="role" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
-                                    <option selected="">Select role</option>
-                                    <option value="user">User</option>
-                                    <option value="admin">Admin</option>
-                                </select>
-                            </div>
-                        </div>
-                        <button type="submit" name="submit" class="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                            <svg class="me-1 -ms-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd"></path></svg>
-                            Add new product
-                        </button>
-                    </form>
-                    <?php
-                    } else {
-                      $name = $_POST["name"];
-                      $email = $_POST["email"];
-                      $password = $_POST["password"];
-                      $role = $_POST["role"];
-
-                      $insertUser = "INSERT INTO user (name, email, password, role) VALUES ('$name', '$email', '$password', '$role')";
-                      $queryUser = mysqli_query($conn, $insertUser);
-
-                      if($queryUser) {
-                        echo"<script>alert('Daftar Berhasil Disimpan !') </script>";  
-                        echo"<script type='text/javascript'>window.location = 'dashboard.php'</script>";  
-                      } else {
-                        echo"<script>alert('Daftar Gagal Disimpan !') </script>";  
-                        echo"<script type='text/javascript'>window.location = 'dashboard.php'</script>";  
-                      }
-                    }
-                    ?>
-                </div>
-            </div>
-        </div>
-
-       
+        </div>    
       </div>
     </div>
   </div>
