8fac0fbf84cbb20ff0fbfbac840ed005b16a7ec9
Cup_Read <hafidh.10523009@mahasiswa.unikom.ac.id>
2025-02-11 17:48:41 +0700

    Delete src/views/user/footer.php

--- src/views/user/footer.php
+++
@@ -1,9 +0,0 @@
-<footer class="absolute bottom-0 left-0 right-0 bg-indigo-600 text-white py-6">
-  <div class="max-w-screen-xl mx-auto px-4 text-center">
-    <h5 class="text-lg font-medium mb-2">
-      <a href="about.php" class="text-white font-medium">
-        Created By Muhammad Raffi, Muhamad Ramdani, Hafidh Adrito Perbawa.
-      </a>
-    </h5>
-  </div>
-</footer>
